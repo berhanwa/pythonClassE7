cf9a694814cda243c1c32cf3c38999c04fb1fc9f
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-10-31 21:59:34 +0000

    Tue, Oct 31, 2023, 9:59 PM +00:00

---
+++ box.py
@@ -0,0 +1,9 @@
+# this is box.py
+
+print ('+__________+')
+print ('|          |')
+print ('|          |')
+print ('|          |')
+print ('|          |')
+print ('|          |')
+print ('+__________+')
