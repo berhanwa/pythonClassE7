c6dd84801c60ca8c46603de7d4ddd0afc285e497
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-11-04 04:34:40 +0000

    Fri, Nov 3, 2023, 11:34 PM -05:00

--- practice.py
+++ practice.py
@@ -19,11 +19,13 @@
 
 # stay active////
 
-def main():
-    print ("She said, \"", end="")
-    print ("Never put off till tomorrow... ", end="")
-    print ("\nWhat you can do")
-    print ("The day", end="")
-    print ()
-    print ("After tomorrow!\"")
-main()
+# def main():
+#     print ("She said, \"", end="")
+#     print ("Never put off till tomorrow... ", end="")
+#     print ("\nWhat you can do")
+#     print ("The day", end="")
+#     print ()
+#     print ("After tomorrow!\"")
+# main()
+
+
