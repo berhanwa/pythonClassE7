4f97553010e27bd9fe610a5aa48afb2b15d00b72
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-11-04 16:58:39 +0000

    Sat, Nov 4, 2023, 11:58 AM -05:00

--- easter.py
+++ easter.py
@@ -3,11 +3,14 @@ print ('Enter a year:')
 y = int(input())
 
 a = y / 19
-b = y // 100, c = y % 100
-d = b // 4, e = b % 4
+b = y // 100
+c = y % 100
+d = b // 4
+e = b % 4
 g = (8 * b + 13) // 25
 h = (19 * a + b - d - g + 15) % 30
-j = c // 4, k = c % 4
+j = c // 4
+k = c % 4
 m = (a + 11 * h) // 319
 r = (2 * e + 2 * j - k - h + m + 32) % 7
 n = (h - m + r + 90) // 25
