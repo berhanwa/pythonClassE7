e1fb154057b35f98a74d1ea86260398286afb791
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-11-04 09:10:21 +0000

    Sat, Nov 4, 2023, 4:10 AM -05:00

--- pset2b/diamond.py
+++ pset2b/diamond.py
@@ -1,7 +1,20 @@
-print('    D    ')
-print('   I I   ')
-print('  A   A  ')
-print('M       M')
+# Made a version where there were '.'s instead of spaces, like we saw in class this week
+
+# print('...D...')
+# print('..I.I..')
+# print('.A...A.')
+# print('M.....M')
+# print('.O...O.')
+# print('..N.N..')
+# print('...D...')
+
+
+# Then copied that version and replaced the '.'s for spaces
+
+print('   D   ')
+print('  I I  ')
+print(' A   A ')
+print('M     M')
 print(' O   O ')
-print('   N  N  ')
-print('    D    ')
+print('  N N  ')
+print('   D   ')
