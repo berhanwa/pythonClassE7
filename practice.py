5458a587d0ba66da885b30bd6eb1a9c91918d34e
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-11-04 06:28:06 +0000

    Sat, Nov 4, 2023, 1:28 AM -05:00

--- practice.py
+++ practice.py
@@ -48,10 +48,9 @@
 # main()
 # print ('\n')
 
-# Complete the following Python code. You may modify only the indicated
-# comment.
-# for j in range (5) :
-#     print (j)
+
+for j in range (5) :
+    print (j)
 # your code goes right here and nowhere else!
 # so that this loop will print the following numbers, one per line:
 # -2 0
@@ -60,5 +59,4 @@
 # 7  3
 # 10 4
 
-for j in range (-2, 11, 3) :
-    print (j)
+
