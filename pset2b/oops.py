86a10497c28887b5fc2f423fec3aebe8c206912b
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-11-04 09:51:26 +0000

    Sat, Nov 4, 2023, 4:51 AM -05:00

--- pset2b/oops.py
+++ pset2b/oops.py
@@ -1,6 +1,10 @@
+# Was a bit of trial and error, but I took my learnings from the temp assignment and added the f'{}' within the print statement
+# Assigned the data type of int to the studs response
+# Also defined the teaching assistants separately and used // instead of %
+# Lastly, I corrected the syntax error on the print by making the P lowercase and corrected the mismatched "'s in the print statement
 def main():
     print ('How many students are enrolled?')
     studs = int(input())
     tf = (studs)//15
-    print ('We need' f'{studs//15} teaching assistants')
+    print ('We need' " " f'{studs//15} teaching assistants')
 main()
