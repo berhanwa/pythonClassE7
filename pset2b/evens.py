1e39fd74973891073c5f0faabb7e8b76c047d49c
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-11-04 10:16:51 +0000

    Sat, Nov 4, 2023, 5:16 AM -05:00

--- pset2b/evens.py
+++ pset2b/evens.py
@@ -1,9 +1,9 @@
-
-
+# After adding the print statement asking users to enter a number, I assigned n to be the input and created the variable for the sum to be used later
 print ('I will compute the sum of even integers from 2 through:')
 n = int(input())
 sum_even = 0
 
+# In the for loop, I specified the range up to n+1 and that values increment by 2 each time
 for i in range(2, n+1, 2):
     sum_even += i
 
