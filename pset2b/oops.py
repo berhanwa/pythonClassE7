18e81f140c718b59ae7ab9ab5da499956a5cdb5a
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-11-04 09:45:48 +0000

    Sat, Nov 4, 2023, 4:45 AM -05:00

--- pset2b/oops.py
+++ pset2b/oops.py
@@ -2,5 +2,5 @@ def main():
     print ('How many students are enrolled?')
     studs = int(input())
     tf = (studs)//15
-    print ('We need,' f'{studs//15} teaching assistants')
+    print ('We need' f'{studs//15} teaching assistants')
 main()
