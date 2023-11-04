1ba8dff67920edb138e6f9517ddcfa62ffae388e
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-11-04 07:52:50 +0000

    Sat, Nov 4, 2023, 2:52 AM -05:00

--- pset2b/temperature.py
+++ pset2b/temperature.py
@@ -2,4 +2,4 @@ print ('Input a Fahrenheit temperature to be converted:')
 fahrenheit = input()
 print (fahrenheit + 'degrees Farenheit equals' + k + "degrees Kelvin")
 
-k=
+k= 5/9(fahrenheit-32)+277.16
