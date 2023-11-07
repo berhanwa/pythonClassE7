eaae9902349d22093f969b97cd199d2394e7cac5
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-11-07 16:51:35 +0000

    Tue, Nov 7, 2023, 10:51 AM -06:00

--- triangle.py
+++ triangle.py
@@ -5,6 +5,6 @@
 for i in range (1, 10):
     line = "" # Makes the loop start fresh when it gets to this point
     for j in range (0, i):
-        line += " " + str(i * 100 + (2 * j * (i - 1))) # Add then assign (+=) the string concatenation of rows by 100 with the even numbers of j multiplied with
-    print (line)
-# The difference in column values increases by even numbers that grow as the column number increases.
+        line += " " + str(i * 100 + (2 * j * (i - 1))) # Add then assign (+=) the string concatenation of rows by 100 with the adjusted even multiples of j.
+    print (line)                                       # The difference in column values increases by even numbers that grow as the column number increases.
+
