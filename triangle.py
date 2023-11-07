5e7044615b29f5af8520112316b5ed8e8fd1a6d7
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-11-07 16:00:59 +0000

    Tue, Nov 7, 2023, 10:00 AM -06:00

--- triangle.py
+++ triangle.py
@@ -1,8 +1,8 @@
 # Use nested loops. Specified i to be the rows, whcih increment by 100
 # And specified j to be the columns, which increases by even numbers
 
-# In the rows loop, set the range to be from 1 -> 10, because the diagram only shows 900 as the last rowing 
+# In the rows loop, set the range to be from 1 -> 10, because the diagram only shows 900 as the last rowing
 for i in range (1, 10):
-    line = ""
+    line = "" # Makes the loop start fresh when it gets to this point
     for j in range (0, i):
-    print ()
+    line += " " + (i * 100 (2 * j * (i - 1))) # Add then assign (+=)
