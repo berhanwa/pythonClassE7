6374ee234efd6244920fb5838f8b08b93a19b990
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-11-07 16:04:38 +0000

    Tue, Nov 7, 2023, 10:04 AM -06:00

--- triangle.py
+++ triangle.py
@@ -4,5 +4,6 @@
 # In the rows loop, set the range to be from 1 -> 10, because the diagram only shows 900 as the last rowing
 for i in range (1, 10):
     line = "" # Makes the loop start fresh when it gets to this point
-    for j in range (0, i):
-    line += " " + (i * 100 (2 * j * (i - 1))) # Add then assign (+=) the concatenation of rows by 100 
+for j in range (0, i):
+    line += " " + str(i * 100 (2 * j * (i - 1))) # Add then assign (+=) the concatenation of rows by 100 and then multiplied with J=j
+    print (line)
