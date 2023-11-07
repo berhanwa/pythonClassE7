ba5037d109d01c34832b87a4ec4610ffd16ff5c0
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-11-07 14:43:58 +0000

    Tue, Nov 7, 2023, 8:43 AM -06:00

--- pset2d/right_triangle.py
+++ pset2d/right_triangle.py
@@ -1,6 +1,8 @@
 # Started by asking user to input the number that will determine both the height and width of the pattern
 height = int(input('How tall do you want the pattern to be:'))
 
-# Then used the range function to set the rows to go down in descending order and inside the loop, 
+# Then used the range function to set the rows to go down in descending order
+# and inside the loop, the negative space " " is printed out as many times as height-i of the range
+# and this is concatenated with the number of * that i specifies per row
 for i in range (height, 0, -1):
     print (" " * (height - i) + "*" * i)
