a50f0ae466101bd5eede9b47e11eaf048092535e
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-11-07 05:23:55 +0000

    Mon, Nov 6, 2023, 11:23 PM -06:00

--- pset2d/batman.py
+++ pset2d/batman.py
@@ -1,11 +1,14 @@
-a = 'na' * 15
-
+# I started by breaking down each component of the program into the following 4 components:
+# Na + a + ... + b
+# I decided that here a and b could be variables (though technically each of the 4 parts could be one), and specified one less for a since the first Na is separate
+a = ' na ' * 15
 b = 'BATMAN!'
 
-complete = 'Na' + a + '...' + b
+# Assigned a variable based on the pseudocode that helped me break down each part of the program, then fed it into the for loop below
+complete = 'Na ' + a + ' ... ' + b
 
-for i in range ():
-    print (complete)
+# for i in range (1):
+#     print (complete)
 
+for
 
-# Na + a + ... + b
