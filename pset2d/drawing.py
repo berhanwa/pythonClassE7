57f54d8f13c82b7d1fccc0073b46257c6d21e1ce
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-11-07 14:59:18 +0000

    Tue, Nov 7, 2023, 8:59 AM -06:00

--- pset2d/drawing.py
+++ pset2d/drawing.py
@@ -6,6 +6,20 @@ top ()
 
 def bottom ():
     print ("\\........./")
-    print (".\\......./.")
-    print (".._______..")
+    print (" \\       / ")
+    print ("  _______  ")
 bottom ()
+
+
+
+# def top ():
+#     print (".._______..")
+#     print ("./.......\\.")
+#     print ("/.........\\")
+# top ()
+
+# def bottom ():
+#     print ("\\........./")
+#     print (".\\......./.")
+#     print (".._______..")
+# bottom ()
