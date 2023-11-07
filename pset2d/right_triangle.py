3d64ca23a680c8c17eefa7e34db15bbb83938a53
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-11-07 04:57:50 +0000

    Mon, Nov 6, 2023, 10:57 PM -06:00

--- pset2d/right_triangle.py
+++ pset2d/right_triangle.py
@@ -1,7 +1,9 @@
-positive_sp = int(input('How tall do you want the pattern to be:'))
+height = int(input('How tall do you want the pattern to be:'))
 # negative_sp = positive_sp
 
 # n =
 
-for i in range (positive_sp):
-    print ('*' * positive_sp)
+for i in range (height):
+    print ('*' * height)
+
+width = height - 
