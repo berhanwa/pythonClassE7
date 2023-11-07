05874171536085c950dd205895a833c195fa8ff2
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-11-07 02:57:48 +0000

    Mon, Nov 6, 2023, 8:57 PM -06:00

--- pset2d/donor.py
+++ pset2d/donor.py
@@ -2,25 +2,9 @@
 n = float(input('Enter the amount of a contribution:'))
 
 
-# def donor():
-#     if n < 15:
-#         print("Cheapskate!")
-#     elif 15 <= n < 200:
-#         print("Friends")
-#     elif 200 <= n < 1000:
-#         print("Supporters")
-#     elif 1000 <= n < 10000:
-#         print("Patrons")
-#     elif n <= 10000:
-#          print("Benefactors")
-#     else:
-#          print("Benefactors")
-# donor()
-
-
 def donor():
     if n < 0:
-        print("Error, please donate a REAL amount (i.e. no coins)!")
+        print("Error, please donate a REAL amount!")
     elif 0 <= n < 15:
         print("Cheapskate!")
     elif 15 <= n < 200:
@@ -32,18 +16,3 @@ def donor():
     else:
          print("Benefactors")
 donor()
-
-
-
-# def donor():
-#     if n < 15:
-#         print("Cheapskate!")
-#     elif 15 <= n < 200:
-#         print("Friends")
-#     elif 200 <= n < 1000:
-#         print("Supporters")
-#     elif 1000 <= n < 10000:
-#         print("Patrons")
-#     else:
-#          print("Benefactors")
-# donor()
