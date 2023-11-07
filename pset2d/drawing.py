c8d344a47d40a5ee9d58340927ca5a8101d4b122
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-11-07 15:12:02 +0000

    Tue, Nov 7, 2023, 9:12 AM -06:00

--- pset2d/drawing.py
+++ pset2d/drawing.py
@@ -1,16 +1,16 @@
 # First, I recreated the shape for both top and bottom definitions with '.'s in place of blank spaces to visualize them (down below)
+# And to print out the actual \s on the right side of the shape, I had to place them twice so they won't be disregarded
 def top ():
     print ("  _______  ")
     print (" /       \\ ")
     print ("/         \\")
-# top ()
 
 def bottom ():
     print ("\\         /")
     print (" \\       / ")
     print ("  _______  ")
-# bottom ()
 
+# Here, I included \s because I was getting an error without them. I believe \ makes the print statement view the -s as text
 def mid ():
     print ("-\"-\'-\"-\'-\"-")
 
