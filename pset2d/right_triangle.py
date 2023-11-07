7bb313e16a908bfea715ac77635ffb34fcfccbd0
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-11-07 14:33:00 +0000

    Tue, Nov 7, 2023, 8:33 AM -06:00

--- pset2d/right_triangle.py
+++ pset2d/right_triangle.py
@@ -1,9 +1,6 @@
+# Started by asking user to input the number that will determine both the height and width of the pattern
 height = int(input('How tall do you want the pattern to be:'))
 
 # n =
-
-for i in range (1, height+1):
-    print ('*' * height)
 for i in range (height, 0, -1):
     print (" " * (height - i) + "*" * i)
-# width = height -
