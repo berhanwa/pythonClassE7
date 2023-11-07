7994103e5a8b412f2551c87a533cb9a746de7ce8
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-11-07 16:25:18 +0000

    Tue, Nov 7, 2023, 10:25 AM -06:00

--- perfect.py
+++ perfect.py
@@ -1,2 +1,12 @@
+# Mental note: perfect numbers equal the sum of their positive divisors. i.e 6 is one, bc divisors 1 + 2 + 3 = 6
 # Started by asking user to input the number that will determine both the height and width of the pattern
-height = int(input('How tall do you want the pattern to be:'))
+
+num = int(input('Enter an integer:'))
+perfect_numbers =
+
+
+def get_divisors (n):
+    divisors = []
+    for i in range (1, n)
+        if n % i == 0:
+            
