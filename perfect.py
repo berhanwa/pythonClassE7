9a6e13307c6dea1e8dfe4f19260d7b7d84924b57
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-11-07 16:29:06 +0000

    Tue, Nov 7, 2023, 10:29 AM -06:00

--- perfect.py
+++ perfect.py
@@ -9,4 +9,5 @@ def get_divisors (n):
     divisors = []
     for i in range (1, n)
         if n % i == 0:
-            
+            divisors.append(i) # Adds the i range value to the divisors
+    return divisors
