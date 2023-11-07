26718feb577a16c216ac1c61591595aea49447fe
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-11-07 14:38:29 +0000

    Tue, Nov 7, 2023, 8:38 AM -06:00

--- pset2d/right_triangle.py
+++ pset2d/right_triangle.py
@@ -1,6 +1,6 @@
 # Started by asking user to input the number that will determine both the height and width of the pattern
 height = int(input('How tall do you want the pattern to be:'))
 
-# Then used the range function to set the rows to go down in descending order and in the print part to 
+# Then used the range function to set the rows to go down in descending order and inside the loop, 
 for i in range (height, 0, -1):
     print (" " * (height - i) + "*" * i)
