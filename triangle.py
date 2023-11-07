5403d9d048f31e360016d1136b8f7bfef74c8264
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-11-07 15:56:20 +0000

    Tue, Nov 7, 2023, 9:56 AM -06:00

--- triangle.py
+++ triangle.py
@@ -1,7 +1,7 @@
 # Use nested loops. Specified i to be the rows, whcih increment by 100
 # And specified j to be the columns, which increases by even numbers
 
-# In the rows loop, set the range to be from 1 -> 10, because the diagram only shows 900 as the last row
+# In the rows loop, set the range to be from 1 -> 10, because the diagram only shows 900 as the last rowing 
 for i in range (1, 10):
     line = ""
     for j in range (0, i):
