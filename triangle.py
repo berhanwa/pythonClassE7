eaad02c59b9c23e0bbdf9edba6dbbacad914711a
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-11-07 16:46:07 +0000

    Tue, Nov 7, 2023, 10:46 AM -06:00

--- triangle.py
+++ triangle.py
@@ -7,3 +7,4 @@ for i in range (1, 10):
     for j in range (0, i):
         line += " " + str(i * 100 + (2 * j * (i - 1))) # Add then assign (+=) the string concatenation of rows by 100 with the even numbers of j multiplied with
     print (line)
+# Increases by even numbers
