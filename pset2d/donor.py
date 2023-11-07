5cc04e03f9eb11db494ff5cf62c0acfea8a36e82
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-11-07 02:15:00 +0000

    Mon, Nov 6, 2023, 8:15 PM -06:00

--- pset2d/donor.py
+++ pset2d/donor.py
@@ -1,12 +1,18 @@
 # print ('Enter the amount of a contribution:')
-n = int(input('Enter the amount of a contribution:'))
-
+n = float(input('Enter the amount of a contribution:'))
 
 
 def donor():
     if n < 15:
         return "Cheapskate!"
-    elif 15 <= n < 200
+    elif 15 <= n < 200:
         return "Friends"
-    elif 200 <= n < 1000
+    elif 200 <= n < 1000:
         return "Supporters"
+    elif 1000 <= n < 10000:
+        return "Patrons"
+    else:
+        return "Benefactors"
+donor()
+
+    # else 10000 <= n:
