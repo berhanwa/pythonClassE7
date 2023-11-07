73b9a0e85634bba9b4eb8e7b6bb0ffa20ec6359c
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-11-07 02:09:17 +0000

    Mon, Nov 6, 2023, 8:09 PM -06:00

--- pset2d/donor.py
+++ pset2d/donor.py
@@ -1,5 +1,10 @@
-print ('Enter the amount of a contribution:')
-n = int(input())
+# print ('Enter the amount of a contribution:')
+n = int(input('Enter the amount of a contribution:'))
+
+
 
 def donor():
-    
+    if n < 15:
+        return "Cheapskate!"
+    elif 15 <= n < 200
+        return "You are a "
