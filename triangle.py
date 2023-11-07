fdeabf85f968d08483b58155c7fe9df6a13c6b4d
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-11-07 15:46:17 +0000

    Tue, Nov 7, 2023, 9:46 AM -06:00

--- triangle.py
+++ triangle.py
@@ -1,6 +1,7 @@
 # Use nested loops. Specified i to be the rows, whcih increment by 100
-# And specified j to be the columns, 
+# And specified j to be the columns,
 
+# Increases by 2 as you go to the right, and increases by 100 as you go down
 for i in range (1, 10):
     line = ""
     for j in range (0, i):
