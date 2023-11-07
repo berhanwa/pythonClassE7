d89ddacb7d39c73fa0b7ed9422ee30938fb9b7c2
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-11-07 16:31:17 +0000

    Tue, Nov 7, 2023, 10:31 AM -06:00

--- perfect.py
+++ perfect.py
@@ -2,12 +2,15 @@
 # Started by asking user to input the number that will determine both the height and width of the pattern
 
 num = int(input('Enter an integer:'))
-perfect_numbers =
+perfect_numbers = [] # Container for the perfect_numbers variable
 
 
 def get_divisors (n):
-    divisors = []
+    divisors = []  # Container for the divisors variable
     for i in range (1, n)
         if n % i == 0:
             divisors.append(i) # Adds the i range value to the divisors
     return divisors
+
+def is_perfect_number (n):
+    
