8736b41e5c9fcc26172e022a667a3834e3217fb5
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-11-11 15:19:23 +0000

    Sat, Nov 11, 2023, 9:19 AM -06:00

--- pset3b/old_man.py
+++ pset3b/old_man.py
@@ -1,3 +1,15 @@
 
+def main():
+    verse (1, "")
+
+
+
 def verse ():
-    print ("This old man, he played", n, " ")
+    print ("This old man, he played", n)
+    print ("He played knick-knack", n)
+    print ("Knick-knack paddy whack")
+    print ("Give the dog a bone")
+    print ("This old man came rolling home.")
+
+
+main ()
