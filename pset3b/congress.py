7597db71716510c958a855c45d2dfc05915b22ea
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-11-11 16:39:34 +0000

    Sat, Nov 11, 2023, 10:39 AM -06:00

--- pset3b/congress.py
+++ pset3b/congress.py
@@ -8,7 +8,7 @@ def eligible_for_senate (age, length_of_citizenship):
     return (age >= 30)  and (length_of_citizenship >= 9)
 
 def main():
-    if eligible_for_house() and eligible_for_senate () True:
+    if eligible_for_house() and eligible_for_senate ():
         print ("The candidate is NOT eligible for election to either the House of Representatives or the Senate.")
     else:
         print ("The candidate is eligible for election to the House of Representatives")
