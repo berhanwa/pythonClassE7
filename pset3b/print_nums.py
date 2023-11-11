6d225980ff62c02f41e3740267741bf05a9da5c0
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-11-11 14:49:39 +0000

    Sat, Nov 11, 2023, 8:49 AM -06:00

--- pset3b/print_nums.py
+++ pset3b/print_nums.py
@@ -12,6 +12,17 @@ def print_range (x,y):
 
 print_range(x,y)
 
+# def print_range (x,y):
+#         if x == y:
+#             print (x)
+#         elif x < y:
+#             for i in range(x, y + 1):
+#                 print (i)
+#         else:
+#             print (i-1)
+
+# print_range(x,y)
+
 
 # def print_range (x,y):
 #     for i in range(x, y + 1):
