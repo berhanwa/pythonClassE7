82767000723f95c8f9f249fa84030596321af47c
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-11-11 16:49:06 +0000

    Sat, Nov 11, 2023, 10:49 AM -06:00

--- pset3b/congress.py
+++ pset3b/congress.py
@@ -1,5 +1,4 @@
-age = int(input('How old are you? '))
-length_of_citizenship = int(input('How many years have you been a US citizen? '))
+
 
 def eligible_for_house (age, length_of_citizenship):
     return (age >= 25)  and (length_of_citizenship >= 7)
@@ -8,12 +7,16 @@ def eligible_for_senate (age, length_of_citizenship):
     return (age >= 30)  and (length_of_citizenship >= 9)
 
 def main():
-    if eligible_for_house() and eligible_for_senate ():
+
+    age = int(input('How old are you? '))
+    length_of_citizenship = int(input('How many years have you been a US citizen? '))
+
+    if eligible_for_house (age, length_of_citizenship) and eligible_for_senate (age, length_of_citizenship):
         print ("The candidate is eligible for election to the House of Representatives AND to the Senate.")
-    elif eligible_for_house() or eligible_for_senate ():
-        if eligible_for_house():
+    elif eligible_for_house(age, length_of_citizenship) or eligible_for_senate (age, length_of_citizenship):
+        if eligible_for_house(age, length_of_citizenship):
             print ("The candidate is eligible for election to the House of Representatives but is NOT eligible for election to the Senate.")
-        if eligible_for_senate():
+        if eligible_for_senate(age, length_of_citizenship):
             print ("The candidate is eligible for election to the Senate but is NOT eligible for election to the House of Representatives.")
     else:
         print ("The candidate is NOT eligible for election to either the House of Representatives or the Senate.")
