4e74fb229cbb2c320c56a973aaf1943c43ec9e8c
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-11-11 15:37:53 +0000

    Sat, Nov 11, 2023, 9:37 AM -06:00

--- pset3b/old_man.py
+++ pset3b/old_man.py
@@ -1,40 +1,15 @@
-verse = (1, "on my thumb")
-verse = (2, "on my shoe")
-verse = (3, "on my knee")
-verse = (4, "on my door")
-verse = (5, "on my hive")
-verse = (6, "with some sticks")
-verse = (7, "up in Heaven")
-verse = (8, "on my gate")
-verse = (9, "on my spine")
-verse = (10, "once again")
-
-def main():
-    def verse (n, str):
-        print ("This old man, he played", n + str)
-        print ("He played knick-knack", n + str)
-        chorus()
-
-    def chorus ():
-        print ("Knick-knack paddy whack")
-        print ("Give the dog a bone")
-        print ("This old man came rolling home.")
-
-main ()
-
+# verse = (1, "on my thumb")
+# verse = (2, "on my shoe")
+# verse = (3, "on my knee")
+# verse = (4, "on my door")
+# verse = (5, "on my hive")
+# verse = (6, "with some sticks")
+# verse = (7, "up in Heaven")
+# verse = (8, "on my gate")
+# verse = (9, "on my spine")
+# verse = (10, "once again")
 
 # def main():
-#     verse (1, "on my thumb")
-#     verse (2, "on my shoe")
-#     verse (3, "on my knee")
-#     verse (4, "on my door")
-#     verse (5, "on my hive")
-#     verse (6, "with some sticks")
-#     verse (7, "up in Heaven")
-#     verse (8, "on my gate")
-#     verse (9, "on my spine")
-#     verse (10, "once again")
-
 #     def verse (n, str):
 #         print ("This old man, he played", n + str)
 #         print ("He played knick-knack", n + str)
@@ -46,3 +21,28 @@ main ()
 #         print ("This old man came rolling home.")
 
 # main ()
+
+
+def main():
+    verse (1, "on my thumb")
+    verse (2, "on my shoe")
+    verse (3, "on my knee")
+    verse (4, "on my door")
+    verse (5, "on my hive")
+    verse (6, "with some sticks")
+    verse (7, "up in Heaven")
+    verse (8, "on my gate")
+    verse (9, "on my spine")
+    verse (10, "once again")
+
+    def verse (n, str):
+        print ("This old man, he played", n + str)
+        print ("He played knick-knack", n + str)
+        chorus()
+
+    def chorus ():
+        print ("Knick-knack paddy whack")
+        print ("Give the dog a bone")
+        print ("This old man came rolling home.")
+
+main ()
