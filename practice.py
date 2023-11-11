88ea7815b10d8a81a9f2c61c7136b95be3342251
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-11-11 06:29:51 +0000

    Sat, Nov 11, 2023, 12:29 AM -06:00

--- practice.py
+++ practice.py
@@ -111,7 +111,25 @@
 # main()
 
 
-def foo(x):
-    return 1 - 2*x
+# def foo(x):
+#     return 1 - 2*x
 
-print ( foo(foo(foo(foo(2)))) )
+# print ( foo(foo(foo(foo(2)))) )
+
+def main():
+    mystery(-2, -6)
+    mystery(2, 3)
+    mystery(4, 8)
+    mystery(10, 31)
+
+
+    def mystery(x, y):
+        s = 0
+        while x > 0 and 2*y >= x:
+            print(s, end=" ")
+            y = y - x
+        x -= 1
+        s = s + 2 * x
+        print(s)
+
+main()
