98e4378789872c6f1cff03a4e1b1c4779c981080
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-11-11 16:56:28 +0000

    Sat, Nov 11, 2023, 10:56 AM -06:00

--- pset3b/increase.py
+++ pset3b/increase.py
@@ -14,3 +14,11 @@ def find_largest_increase ():
                 end_day = day
 
                 prev_price = price
+
+            if max_increase > 0:
+        print(f"Largest increase of {max_increase} from {prev_price - max_increase} to {prev_price} occurred between day #{start_day} and day #{end_day}.")
+    else:
+        print("No increase in stock price during the given period.")
+
+# Example usage:
+find_largest_increase()
