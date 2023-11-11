a395301436e7983abcf3bb5d374a41c674de49ba
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-11-11 15:24:16 +0000

    Sat, Nov 11, 2023, 9:24 AM -06:00

--- pset3b/old_man.py
+++ pset3b/old_man.py
@@ -1,17 +1,24 @@
 
 def main():
-    verse (1, "")
+    verse (1, "on my thumb")
+    verse (2, "on my shoe")
+    verse (3, "on my knee")
+    verse (4, "on my door")
+    verse (5, "on my hive")
+    verse (6, "with some sticks")
+    verse (7, "up in Heaven")
+    verse (8, "on my gate")
+    verse (9, "on my spine")
+    verse (10, "once again")
+
+    def verse (n ):
+        print ("This old man, he played", n)
+        print ("He played knick-knack", n)
 
 
-
-def verse ():
-    print ("This old man, he played", n)
-    print ("He played knick-knack", n)
-
-
-def chorus ():
-    print ("Knick-knack paddy whack")
-    print ("Give the dog a bone")
-    print ("This old man came rolling home.")
+    def chorus ():
+        print ("Knick-knack paddy whack")
+        print ("Give the dog a bone")
+        print ("This old man came rolling home.")
 
 main ()
