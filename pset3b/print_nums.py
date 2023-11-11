2ba3de57c6e5e8116ae42710e42b2c970dfdb446
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-11-11 14:25:36 +0000

    Sat, Nov 11, 2023, 8:25 AM -06:00

--- pset3b/print_nums.py
+++ pset3b/print_nums.py
@@ -1,8 +1,13 @@
 x = int(input('Enter the first number:'))
 y = int(input('Now enter the second number:'))
 
-def print_range ():
+def print_range (x,y):
+    if x == y:
+        print (x)
+    elif x < y:
+        print (x += 1)
+
+print_range()
+
     while x < y:
         print (x += 1)
-        # break
-print_range()
