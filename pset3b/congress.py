17bc2889c3af187f562f9030a0ddd5af44ed1ef0
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-11-11 16:10:37 +0000

    Sat, Nov 11, 2023, 10:10 AM -06:00

--- pset3b/congress.py
+++ pset3b/congress.py
@@ -1,4 +1,6 @@
-age = int(input('How old are you?'))
-length_of_citizenship = int(input('How many years have you been a US citizen?'))
+age = int(input('How old are you? '))
+length_of_citizenship = int(input('How many years have you been a US citizen? '))
 
-
+def eligible_for_house (age, length_of_citizenship):
+    if age >= 25 and length_of_citizenship >= 7:
+        return True
