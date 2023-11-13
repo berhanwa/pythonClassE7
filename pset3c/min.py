9ae756a6e717ecd133dad7f2c3e7851befdf553a
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-11-13 18:41:33 +0000

    Mon, Nov 13, 2023, 12:41 PM -06:00

--- pset3c/min.py
+++ pset3c/min.py
@@ -1,9 +1,9 @@
-# Returns the integer that has the smallest value between x, y, and z.
+# Started by assigning the input values to x, y, and z respectively.
 x = int(input('Enter the first number: '))
 y = int(input('Enter the second number: '))
 z = int(input('Enter the third number: '))
 
-# Returns the integer that has the smallest value between x, y, and z.
+# Defined the function to return the integer with the smallest value between x, y, and z.
 def my_min(x, y, z):
     if (x < y and x < z):
         return x
@@ -12,7 +12,10 @@ def my_min(x, y, z):
     else:
         return z
 
+# Then used print to actually show what the return from the function will be.
 print (my_min(x, y, z))
 
+
+# Removed this code from the if/else since it is implied
     # else (z < x and z < y):
     #     return z
