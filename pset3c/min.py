dfd375498c4a6878e887b70ba04c68ba5350219a
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-11-13 18:27:56 +0000

    Mon, Nov 13, 2023, 12:27 PM -06:00

--- pset3c/min.py
+++ pset3c/min.py
@@ -3,4 +3,5 @@ y = int(input('Enter the second number: '))
 z = int(input('Enter the third number: '))
 
 # Returns the integer that has the smallest value between x, y, and z.
-def my_min(x, y):
+def my_min(x, y, z):
+    if (x < y)
