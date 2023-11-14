93605bced08819bba2abcc6f6009cd3e61ee8616
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-11-14 22:27:57 +0000

    Tue, Nov 14, 2023, 4:27 PM -06:00

--- pset3d/drunk.py
+++ pset3d/drunk.py
@@ -1,7 +1,9 @@
 import random
 
+start = 6
+
 def drunk_walk():
-    steps = random.randint(1, 5)
+    steps = random.randint(1, 11)
     while steps ==
         # simulates drunk walk
         # starts at 6, can randomly go either down to 1 or up to 11
