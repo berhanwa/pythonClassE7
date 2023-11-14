769304eb1a5de15652641c17689acd8efb5d2797
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-11-14 06:16:42 +0000

    Tue, Nov 14, 2023, 12:16 AM -06:00

--- pset3c/craps.py
+++ pset3c/craps.py
@@ -1,9 +1,10 @@
-roll 2 dice
 
+# roll 2 dice
 def do_roll():
     roll_1 = int(input('Roll the first die! '))
     roll_2 = int(input('Now roll the second die! '))
-    return sum of roll
+    return sum of rolls
 
 def print_roll(roll_1, roll_2):
+    print ()
 
