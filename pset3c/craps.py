07ccd2db1ba78a5b4fd721a9153cd91d6217c208
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-11-14 17:16:05 +0000

    Tue, Nov 14, 2023, 11:16 AM -06:00

--- pset3c/craps.py
+++ pset3c/craps.py
@@ -9,27 +9,26 @@ def do_roll():
 
 # print_roll() function prints the results of the rolls
 def print_roll(roll_1, roll_2):
-    print (f"Computer rolls a {roll_1} and a {roll_2} )
+    print (f"Computer rolls a {roll_1} and a {roll_2}, for a total of {roll_1 + roll_2}.")
 
+# get_point() function finds the point for the first round and returns it (first while loop is used here)
+def get_point():
+    point = 0
 
-# def get_point():
+    while True:
+        roll = do_roll()
 
-#     # while True:
+#       Conditions for point to be met
+        if roll >= 4 and roll != 7 and roll <= 10:
+            point = roll
+            break
 
-#     if roll_sum == 4 or 5 or 6 or 8 or 9 or 10:
-#         roll_sum = int(point)
-#     elif do_roll() == 7:
-#         return 0   # stop immediately
-#         # print ("YOU LOSE")
+    return point
 
-#     takes no input
-#     rolls until point is est
-#     return val of point
-
-
-# def play_from_point(point):
-#     while point != 7:
-        # keep playing
+# play_from_point() function finds the point for the first round and returns it (first while loop is used here)
+def play_from_point(point):
+    while point != 7:
+        keep playing
 
 #     takes point val
 #     cont game until W or L
