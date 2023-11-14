34e9f658c98551b161dd50bc3883bfeb26021e2c
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-11-14 23:20:07 +0000

    Tue, Nov 14, 2023, 5:20 PM -06:00

--- pset3d/drunk.py
+++ pset3d/drunk.py
@@ -21,7 +21,6 @@ print (block)
 #   # steps ==
 #         # simulates drunk walk
 #         # starts at 6, can randomly go either down to 1 or up to 11
-#         return total blocks walked
 
 # def main():
 #     # this process is repeated N times
