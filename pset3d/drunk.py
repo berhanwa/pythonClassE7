28fee1bc06c62e7e4cf29e3fdf09e8c58ab92e18
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-11-14 22:51:47 +0000

    Tue, Nov 14, 2023, 4:51 PM -06:00

--- pset3d/drunk.py
+++ pset3d/drunk.py
@@ -1,27 +1,28 @@
 import random
 
-end_block variable HOME if 1
-end_block variable JAIL if 11
+# end_block variable HOME if 1
+# end_block variable JAIL if 11
 
 start_block = 6
 
 def drunk_walk():
 
     block = random.randint(1, 11)
-    while True:
-        if block == 1:
-            print ("HOME")
-        elif block == 11:
-            print ("JAIL")
+        while True:
+            if block == 1:
+                print ("HOME")
+            elif block == 11:
+                print ("JAIL")
+            break
 
-    return 
+    return block
 
-    steps ==
-        # simulates drunk walk
-        # starts at 6, can randomly go either down to 1 or up to 11
-        return total blocks walked
+#   # steps ==
+#         # simulates drunk walk
+#         # starts at 6, can randomly go either down to 1 or up to 11
+#         return total blocks walked
 
-def main():
-    # this process is repeated N times
-    for i in range (N):
-        steps = drunk_walk
+# def main():
+#     # this process is repeated N times
+#     for i in range (N):
+#         steps = drunk_walk
