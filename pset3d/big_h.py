dc7a7aed2f6fab6ab64e4c5a20a033262d3b4a8b
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-11-15 03:46:38 +0000

    Tue, Nov 14, 2023, 9:46 PM -06:00

--- pset3d/big_h.py
+++ pset3d/big_h.py
@@ -1,13 +1,15 @@
 def print_h_block(size):
 
+    # Setting the parameters to ensure users only submit a value between 1 and 10 in order to see an 'H'
     if size < 1 or size > 10:
         print("Please enter a number between 1 and 10.")
         return
 
+    # Setting the parameters to ensure users only submit a value between 1 and 10 in order to see an 'H'
     for i in range(2*size + 1):
         if i == size :
             for j in range(i):
-                print('*' * size + '*' * size '*' * size)
+                print('*' * size + '*' * size + '*' * size)
 
         else:
             print('*' * size + ' ' * size + '*' * size)
