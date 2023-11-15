55bed5a00e0146547bbeb265e62688bf7d54df2b
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-11-15 01:50:40 +0000

    Tue, Nov 14, 2023, 7:50 PM -06:00

--- pset3c/credit_cards.py
+++ pset3c/credit_cards.py
@@ -2,7 +2,7 @@
 
 # From right -> left, find the sum of every other digit
 
-cc_number = int(input('Provide your credit card number: '))
+card_number = int(input('Provide your credit card number: '))
 alt_sum = 0
 double = 0
 
@@ -11,13 +11,18 @@ for i in range (1,9):
         alt_sum += i
     else:
         double += i
-    cc_number = cc_number//10
+    card_number = card_number//10
 
 total_sum(alt_sum, double):
     return ()
 
  doubles_sum = (2 * double):
 # if 2 * double results in a double digit number, then split up into single digits i.e. 18 becomes 1 and 8. Then need to sum everything up together
+
+def check(card_number):
+    digits = []
+    for d in str(card_number):
+        digits.append(int(d))
 
 # Returns the sum of the integers 1 through n. (from textbook)
 # def sum_of(n):
