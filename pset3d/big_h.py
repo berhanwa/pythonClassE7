30a6512487b4de20712ca31d94b410e16bf5fbcf
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-11-15 04:05:41 +0000

    Tue, Nov 14, 2023, 10:05 PM -06:00

--- pset3d/big_h.py
+++ pset3d/big_h.py
@@ -12,14 +12,14 @@ def print_h_block(size):
              # Prints middle row as many times as size is set to
             for j in range(i):
 
-                # Set * 3 times because in the example , there is a correlation between 3 rows being multiplied to the # of size
+                # Set * 3 times because there is a correlation between 3 rows being multiplied to the # of size
                 # Middle row has * as the middle column instead of ' '
                 print('*' * size + '*' * size + '*' * size)
 
-         # Prints * and negative spaces as many times as size is set to
+         # Prints * and ' ' as many times as size is set to
         else:
             print('*' * size + ' ' * size + '*' * size)
 
 
-user_input = int(input("Please enter a number (1-10) for the size of the letter 'H'"))
+user_input = int(input("Please enter a number between 1 and 10 for the size of the letter 'H' "))
 print_h_block(user_input)
