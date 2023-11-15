ad0e61bcde56f55239828dd98ddddd61ff3b734b
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-11-15 01:57:06 +0000

    Tue, Nov 14, 2023, 7:57 PM -06:00

--- pset3c/credit_cards.py
+++ pset3c/credit_cards.py
@@ -24,7 +24,7 @@ def check(card_number):
     for d in str(card_number):
         digits.append(int(d))
 
-    # len(digits)-1
+    # len(digits)-1 means I'm checking for the 8th digit
     check_digit = digits[len(digits)-1]
 
 # Returns the sum of the integers 1 through n. (from textbook)
