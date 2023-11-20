f606e9177cc7661834e342eeaf8f2be7f7561295
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-11-20 18:41:08 +0000

    Mon, Nov 20, 2023, 12:41 PM -06:00

--- pset4b/vowels.py
+++ pset4b/vowels.py
@@ -1,7 +1,9 @@
 def vowel_count(sentence):
-    sentence.count('aeiouAEIOU')
-print (vowel_count(sentence))
+    # sentence.count('aeiouAEIOU')
+    for i in range (0, len(sentence)):
+        
 
 
 
 sentence = "I think, therefore I am"
+print (vowel_count(sentence))
