5911b2378d35406dd1cb9c8eda71a7f722ac3db4
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-11-20 23:31:51 +0000

    Mon, Nov 20, 2023, 5:31 PM -06:00

--- pset4b/license.py
+++ pset4b/license.py
@@ -2,24 +2,23 @@ import random
 import string
 
 
+# def random_plate():
 def random_capital():
     letters = [random.choice(string.ascii_uppercase) for _ in range (3)]
     return ''.join(letters)
 
 
-# print(random_capital())
-
-
+# Defined the random_plate function where random numbers are generated, and with the f string, combined the letters and a space to form the overall license plates
 def random_plate():
     numbers = random.randint(100, 999)
     license = f"{random_capital()} {numbers}"
     return license
 
-# Here, the results are being printed
+# Here, the results are being printed 20 times within this for loop that has _ as the placeholder variable that gets ignored
 for _ in range (20):
     print(random_plate())
 
-# def random_plate():
+
 #     license = ".join( letters + " " + random.randint(100,999) )"
 #     return license
 # print (random_plate())
