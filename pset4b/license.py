3f524e2054b67c587ea0743feb49a28c715a1ca0
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-11-20 23:34:30 +0000

    Mon, Nov 20, 2023, 5:34 PM -06:00

--- pset4b/license.py
+++ pset4b/license.py
@@ -2,7 +2,7 @@ import random
 import string
 
 
-# def random_plate():
+# Defined the random_capital function where random capital letters are generated
 def random_capital():
     letters = [random.choice(string.ascii_uppercase) for _ in range (3)]
     return ''.join(letters)
@@ -17,10 +17,3 @@ def random_plate():
 # Here, the results are being printed 20 times within this for loop that has _ as the placeholder variable that gets ignored
 for _ in range (20):
     print(random_plate())
-
-
-#     license = ".join( letters + " " + random.randint(100,999) )"
-#     return license
-# print (random_plate())
-
-
