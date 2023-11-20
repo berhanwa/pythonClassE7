8dd6fc32787168e7d2858c42470f337b49108b76
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-11-20 19:41:09 +0000

    Mon, Nov 20, 2023, 1:41 PM -06:00

--- pset4b/vowels.py
+++ pset4b/vowels.py
@@ -2,11 +2,12 @@ def vowel_count(sentence):
     # sentence.count('aeiouAEIOU')
     count = 0
     for i in range (len(sentence)):
-        if "a" or "e" or "i" or "o" or "u" in sentence:
+        if vowels in sentence:
             return count += 1
 
         # sentence.count(x) for x in "aeiouAEIOU":
     # return vowel_count(sentence)
 
+vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
 sentence = "I think, therefore I am"
 print (vowel_count(sentence))
