cdb873cefdfffc985eb164a69dda0606ca0690ad
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-11-20 22:40:39 +0000

    Mon, Nov 20, 2023, 4:40 PM -06:00

--- pset4b/license.py
+++ pset4b/license.py
@@ -1,16 +1,4 @@
-import random
-import string
-
-def random_capital():
-    letters = random.choice(string.ascii_uppercase)
-    return letters
-print (random_capital())
-
-def random_plate():
-    numbers = random.randint(100,999)
-    license = f"{letters} {numbers}"
-    return license
-print (random_plate())
+xc
 
 # def random_plate():
 #     license = ".join( letters + " " + random.randint(100,999) )"
