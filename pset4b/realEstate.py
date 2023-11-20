36837dfd2d61c576c65ae95951c0b7c3a4dfdd8d
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-11-20 16:05:34 +0000

    Mon, Nov 20, 2023, 10:05 AM -06:00

--- pset4b/realEstate.py
+++ pset4b/realEstate.py
@@ -3,7 +3,7 @@ def de_vowel (ad):
         # if vowel = is_vowel(ad[i])
         if is_vowel(c) and is_vowel(ad[i]):
             ad = ad[:i] + ad [i+1:]
-        return ad
+    return ad
 
 def is_vowel(c):
     if c in 'aeiouAEIOU':
