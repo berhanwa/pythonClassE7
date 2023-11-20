3d44c31407e5d1f050de9406be1ecc4260715a42
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-11-20 16:11:25 +0000

    Mon, Nov 20, 2023, 10:11 AM -06:00

--- pset4b/realEstate.py
+++ pset4b/realEstate.py
@@ -1,7 +1,8 @@
 def de_vowel(ad):
     for i in range (1, len(ad)):
         # if vowel = is_vowel(ad[i])
-        if is_vowel(c) and is_vowel(ad[i]):
+        # if is_vowel(c) and is_vowel(ad[i]):
+        if is_vowel(ad[i]):
             ad = ad[:i] + ad [i+1:]
     return ad
 
