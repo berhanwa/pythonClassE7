7b9c1d2afe369ed3febe898db2096b4a87729ab2
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-11-21 03:43:02 +0000

    Mon, Nov 20, 2023, 9:43 PM -06:00

--- pset4b/realEstate.py
+++ pset4b/realEstate.py
@@ -1,22 +1,18 @@
-# Defined the function is_vowel to return true or false depending on whether or not the character has a vowel
+# Defined the de_vowel function where in the for loop that runs through the ad,
+# checks if there are vowels and then puts the remaining characters inside the vowelless_ad variable
+def de_vowel(ad):
+    vowelless_ad = ''
+    for i in range (0, len(ad)):
+        if not(is_vowel(ad[i]) and i != 0 and ad[i-1]!=' '):
+            vowelless_ad += ad[i]
+    return vowelless_ad
+
+# Then defined the function is_vowel to return true or false depending on whether or not the character has a vowel
 def is_vowel(c):
     if c in 'aeiouAEIOU':
         return True
     else:
         return False
 
-# Then defined the de_vowel function where in the for loop that runs through the ad,
-# checks if there are vowels and then puts the remaining characters inside the vowelless_ad variable
-def de_vowel(ad):
-    vowelless_ad = ''
-    for i in range (0, len(ad)):
-        if not(is_vowel(ad[i]) and i != 0 and ad[i-1]!=' '):
-            # ad += vowelless_ad
-            # vowelless_ad = vowelless_ad[:i] + vowelless_ad[i+1:]
-            vowelless_ad += ad[i]
-    return vowelless_ad
-
-
 ad = "Desirable unfurnished flat in quiet residential area"
 print (de_vowel(ad))
-
