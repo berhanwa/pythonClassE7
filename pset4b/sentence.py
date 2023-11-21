21aa48722d882bb0b99f5a8996037ef9620aa6dc
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-11-21 01:11:27 +0000

    Mon, Nov 20, 2023, 7:11 PM -06:00

--- pset4b/sentence.py
+++ pset4b/sentence.py
@@ -2,5 +2,15 @@ def sentence_type (input):
 input = "This is a sentence."
 
 
+def vowel_count(sentence):
+    count = 0
+    for i in sentence:
+        if i in vowels:
+            count += 1
+    return count
+
 def main():
 
+
+
+print(f"Your entry has {vowel_count(sentence)} vowels in it")
