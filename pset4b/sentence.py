6d92b0f719cbe294954a5017211a1bd96920703d
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-11-21 02:12:30 +0000

    Mon, Nov 20, 2023, 8:12 PM -06:00

--- pset4b/sentence.py
+++ pset4b/sentence.py
@@ -1,13 +1,18 @@
-def sentence_type(input):
+def sentence_type(entry):
     # for i in range(len(input)):
-    if input[len - 1] == ".":
-        print("declarative")
-
+    if entry[- 1] == ".":
+        return "declarative"
+    elif entry[- 1] == "!":
+        return "exclamatory"
+    elif entry[- 1] == "?":
+        return "interrogative"
+    else:
+        return "bad ending"
 
 print(sentence_type(input))
 
-# def main():
+def main():
 
 
-input = "This is a sentence."
+entry = "This is a sentence."
 # print(f"Your entry has {vowel_count(sentence)} vowels in it")
