8cf86a10c49bb7eb7a6d0fca1a69768858f5ddc0
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-11-21 03:33:02 +0000

    Mon, Nov 20, 2023, 9:33 PM -06:00

--- pset4b/realEstate.py
+++ pset4b/realEstate.py
@@ -11,9 +11,10 @@ def de_vowel(ad):
         # if vowel = is_vowel(ad[i])
         # if is_vowel(c) and is_vowel(ad[i]):
         # if is_vowel(ad[i]) and ad[i-1]!=' ':
-        if is_vowel(c) and i != 0 and ad[i-1]!=' ':
+        if not(is_vowel(c) and i != 0 and ad[i-1]!=' '):
             # ad += vowelless_ad
-            vowelless_ad = vowelless_ad[:i] + vowelless_ad[i+1:]
+            # vowelless_ad = vowelless_ad[:i] + vowelless_ad[i+1:]
+            vowelless_ad += ad[:i]
     return vowelless_ad
 
 
