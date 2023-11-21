d80a19809f8b27a4c34c3c73856888128aa3b20c
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-11-21 01:55:17 +0000

    Mon, Nov 20, 2023, 7:55 PM -06:00

--- pset4b/sentence.py
+++ pset4b/sentence.py
@@ -1,11 +1,12 @@
 def sentence_type (input):
-    for i in range(len(input)):
-        if i
+    # for i in range(len(input)):
+        if input[len-1] == ".":
+            print('declarative')
 
+print(sentence_type (input))
 
-
-def main():
+# def main():
 
 
 input = "This is a sentence."
-print(f"Your entry has {vowel_count(sentence)} vowels in it")
+# print(f"Your entry has {vowel_count(sentence)} vowels in it")
