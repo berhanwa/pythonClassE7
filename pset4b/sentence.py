17a80a2617f17c797085ee47f4ae722d39f14768
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-11-21 01:41:37 +0000

    Mon, Nov 20, 2023, 7:41 PM -06:00

--- pset4b/sentence.py
+++ pset4b/sentence.py
@@ -3,12 +3,6 @@ def sentence_type (input):
         if i
 
 
-def vowel_count(sentence):
-    count = 0
-    for i in sentence:
-        if i in vowels:
-            count += 1
-    return count
 
 def main():
 
