538a565e438e72dcdec3c36b1590e1b25aebadb2
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-11-21 03:24:43 +0000

    Mon, Nov 20, 2023, 9:24 PM -06:00

--- pset4b/realEstate.py
+++ pset4b/realEstate.py
@@ -1,19 +1,22 @@
-def de_vowel(ad):
-    vowelless_ad = ' '
-    for i in range (1, len(ad)):
-        # if vowel = is_vowel(ad[i])
-        # if is_vowel(c) and is_vowel(ad[i]):
-        if is_vowel(ad[i]) and ad[i-1]!=' ':
-            # ad += vowelless_ad
-            vowelless_ad = vowelless_ad[:i] + vowelless_ad[i+1:]
-    return vowelless_ad
-
 def is_vowel(c):
     # c = ' '
     if c in 'aeiouAEIOU':
         return True
     else:
         return False
+
+def de_vowel(ad):
+    vowelless_ad = ' '
+    for i in range (1, len(ad)):
+        # if vowel = is_vowel(ad[i])
+        # if is_vowel(c) and is_vowel(ad[i]):
+        # if is_vowel(ad[i]) and ad[i-1]!=' ':
+        if is_vowel(c) and ad[i-1]!=' ':
+            # ad += vowelless_ad
+            vowelless_ad = vowelless_ad[:i] + vowelless_ad[i+1:]
+    return vowelless_ad
+
+
 
 ad = "Desirable unfurnished flat in quiet residential area"
 print (de_vowel(ad))
