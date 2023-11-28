aeb4bdf79ab048055353972e024a97a7f1b697ed
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-11-28 18:04:21 +0000

    Tue, Nov 28, 2023, 12:04 PM -06:00

--- pset5c/repeated_digits.py
+++ pset5c/repeated_digits.py
@@ -11,9 +11,8 @@ def digit_check(numbers):
 
     return occurrences
 
-digit_check(numbers) = outcome
-
 numbers = str(int(input('Please enter some numbers: ')))
+outcome = digit_check(numbers)
 print(f"Digit: 0 1 2 3 4 5 6 7 8 9 \nOccurrences: {''.join(map(outcome))}")
 
 # print(f"Digit: 0 1 2 3 4 5 6 7 8 9 \nOccurrences: {digit_check(numbers)}\n")
