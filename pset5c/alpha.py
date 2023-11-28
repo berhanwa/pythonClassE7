7fe388491c429885b39d3ea4f50d2ea873a67682
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-11-28 18:59:52 +0000

    Tue, Nov 28, 2023, 12:59 PM -06:00

---
+++ pset5c/alpha.py
@@ -0,0 +1,11 @@
+# This problem is attempted for extra credit :)
+
+words = ['apple', 'pumpkin', 'log', 'River', 'fox', 'pond']
+
+def alphabetical(words):
+
+    for word in words:
+
+
+    print()
+    print(f"Original: \t0 1 2 3 4 5 6 7 8 9 \nOccurrences: \t{' '.join(map(str, outcome))}")
