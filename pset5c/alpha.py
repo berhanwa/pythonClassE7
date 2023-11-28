6bcf32df906d46b5a335365d9356bc7b97eeea7e
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-11-28 19:13:23 +0000

    Tue, Nov 28, 2023, 1:13 PM -06:00

--- pset5c/alpha.py
+++ pset5c/alpha.py
@@ -8,6 +8,8 @@ def alphabetical(words):
     for word in words:
         sorted_words.append(' '.join(sorted(word)))
 
-# Then printed the original list of words
+# Then printed the original list of words with the similar tabbed formatting I used in problem 8, and printed the same list of words in alphabetical order
     print(f"Original: \t", ' '.join(words))
     print(f"Aplhabetical: \t", ' '.join(sorted_words))
+
+alphabetical(words)
