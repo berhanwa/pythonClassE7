5c6d9bc73ce2b5ee1135c04718747a1b9d8ade09
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-11-28 08:35:39 +0000

    Tue, Nov 28, 2023, 2:35 AM -06:00

--- pset5b/lowest_grade.py
+++ pset5b/lowest_grade.py
@@ -1,23 +1,31 @@
+# Defined the functionn by first addressing the cases where no grades are passed and if only one grade is passed with if statements
 def remove_lowest(grades):
-    # count = [0, 0, 0, 0, 0]
-    lowest_index = grades.index(min(grades))
-
-    grades.pop(lowest_index)
 
     if grades == []:
         return []
     elif len(grades) == 1:
         return grades
 
+    # Then addressed the part where the index of the lowest grade is specified, with the help of the min() list method
+    lowest_index = grades.index(min(grades))
+
+    # Then removed said index of the lowest grade with the help of the list.pop() list method
+    grades.pop(lowest_index)
+
     return grades
 
+
+# Finally, printed out the grades listed from the PSET examples to thoroughly test the program
 grades = [23, 90, 47, 55, 88]
 print("a =", remove_lowest(grades))
 
 
 grades = [85]
-print("a =", remove_lowest(grades))
+print("b =", remove_lowest(grades))
 
 
 grades = []
-print("a =", remove_lowest(grades))
+print("c =", remove_lowest(grades))
+
+grades = [59, 92, 93, 47, 88, 47]
+print("d =", remove_lowest(grades))
