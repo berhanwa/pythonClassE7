7b7325ac699f48946e4c4fb0028c4ce6031cafdb
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-11-28 18:50:02 +0000

    Tue, Nov 28, 2023, 12:50 PM -06:00

--- pset5c/repeated_digits.py
+++ pset5c/repeated_digits.py
@@ -3,7 +3,7 @@ def digit_check(numbers):
     # Initialized the occurences list and multiplied by 10 since there will be 10 values inside
     occurrences = [0] * 10
 
-    # Here, the for loop iterates through the parameter where the elements are assigned to integers and are then incremented inside the occurrences loop
+    # Here, the for loop iterates through the parameter where the elements are assigned to integers and are then incremented inside the occurrences List
     for i in numbers:
         digit = int(i)
         occurrences[digit] += 1
