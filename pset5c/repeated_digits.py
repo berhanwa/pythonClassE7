fb56ff6771af0f22aaa54d59eb03cc456eb5c2e5
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-11-28 18:18:54 +0000

    Tue, Nov 28, 2023, 12:18 PM -06:00

--- pset5c/repeated_digits.py
+++ pset5c/repeated_digits.py
@@ -17,7 +17,7 @@ numbers = str(int(input('Please enter some numbers: ')))
 outcome = digit_check(numbers)
 
 # Used tab to get the two sets of numbers to align vertically, and \n to start a new line
-# And to print out the results of the function, I used the 
+# And to print out the results of the function, I used the join and map functions to
 print(f"Digit: \t\t0 1 2 3 4 5 6 7 8 9 \nOccurrences: \t{' '.join(map(str, outcome))}")
 
 
