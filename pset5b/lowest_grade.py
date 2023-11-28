61eb8cf1133398a7bf8c907fa0aa36ee54f9f154
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-11-28 08:22:57 +0000

    Tue, Nov 28, 2023, 2:22 AM -06:00

--- pset5b/lowest_grade.py
+++ pset5b/lowest_grade.py
@@ -1,7 +1,10 @@
 def remove_lowest(grades):
     # count = [0, 0, 0, 0, 0]
     lowest_index = grades.index(min(grades))
-    
+
+    grades.pop(lowest_index)
+
+    return grades
 
 grades = [90, 47, 55, 88]
-preint("a =", grades)
+print("a =", grades)
