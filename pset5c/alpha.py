9531643a3ab0d9c72ffe99dd7b899b61be464b95
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-11-28 19:20:22 +0000

    Tue, Nov 28, 2023, 1:20 PM -06:00

--- pset5c/alpha.py
+++ pset5c/alpha.py
@@ -3,13 +3,14 @@ words = ['apple', 'pumpkin', 'log', 'River', 'fox', 'pond']
 
 def alphabetical(words):
 
-    # Initialized the sorted_words list
+    # Initialized sorted_words as an empty list
     sorted_words = []
 
+    # Here, the for loop iterates through the parameter where the words are each sorted alphabetically with the sorted function and appended into the sorted_words list that was just initialized earlier
     for word in words:
         sorted_words.append(''.join(sorted(word)))
 
-# Then printed the original list of words with the similar tabbed formatting I used in problem 8, and printed the same list of words in alphabetical order
+    # Then printed the original list of words with the similar tabbed formatting and join method to concatenate the list into strings I used in problem 8, and printed the sorted_words list in the same way
     print(f"Original: \t", ' '.join(words))
     print(f"Aplhabetical: \t", ' '.join(sorted_words))
 
