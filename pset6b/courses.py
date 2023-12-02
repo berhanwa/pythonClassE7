919c717b1a9bdcbe69712b8b26e0425977a63e43
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-12-02 00:00:37 +0000

    Fri, Dec 1, 2023, 6:00 PM -06:00

---
+++ pset6b/courses.py
@@ -0,0 +1,20 @@
+courses = {
+    "CSCI-E7" :
+    { "full_name" : "Intro to CS with Python",
+        "instructors" : [ "Henry", "Foobar" ],
+        "TAs" : ["Alyssa", "Apekshya", "Ben",
+        "Arpit", "Stephen"],
+        "num_homeworks" : 7,
+        "num_exams" : 2
+    },
+
+    "FREN-E-1b" :
+    { "full_name" : "Elementary French I",
+        "instructors" : [ "Taieb" ],
+        "tfs" : ["Gloria", "Mona"],
+        "num_homeworks" : 12,
+        "num_exams" : 2
+    }
+}
+
+def 
