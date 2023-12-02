c02bde5b0c417455366a3d74a63df9266183d071
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-12-02 00:20:06 +0000

    Fri, Dec 1, 2023, 6:20 PM -06:00

--- pset6b/courses.py
+++ pset6b/courses.py
@@ -18,9 +18,15 @@ courses = {
 
 def total_homeworks(course_list):
 
+    # Initialized this variable to set the stage
     total_work = 0
+
+    # Used a for loop to iterate through the keys of the course_list dictionary, with i representing each course
     for i in course_list:
+
+        # The 'num_homeworks' keys' values are collected and then added on to total_work variable, which lets us know how much homework there is to do
         total_work += course_list[i]['num_homeworks']
+        
     return total_work
 
 
