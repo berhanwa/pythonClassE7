83e285008938cc72e5cf24bb7ce90fd9b2f04d99
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-12-05 07:50:43 +0000

    Tue, Dec 5, 2023, 1:50 AM -06:00

--- pset6d/find.py
+++ pset6d/find.py
@@ -5,10 +5,12 @@ input_file_name = input("Enter a python file to search it in: ")
 f = open (input_file_name, "r")
 
 line = f.readline()
+number_of_line = 0
 
 while len(line) != 0:
     if keyword_name in line:
-        print (line, end='')
+        number_of_line += 1
+        print (number_of_line, end='')
     line = f.readline()
 
 f.close()
