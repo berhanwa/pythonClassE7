2e16697baafb0e2a5ea89919f096d3936fd6cdea
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-12-05 02:58:42 +0000

    Mon, Dec 4, 2023, 8:58 PM -06:00

--- pset6c/authorship.py
+++ pset6c/authorship.py
@@ -12,7 +12,7 @@ def word_length_histogram(text):
                 word_histogram(len(word)) += 1
 
             else:
-                
+                word_histogram(len(word)) = 1
 
 
 def print_report(hist):
