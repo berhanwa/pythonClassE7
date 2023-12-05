9e1dc3afe6f68ec447cdf2c3b68ca4f754beb8c8
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-12-05 03:40:32 +0000

    Mon, Dec 4, 2023, 9:40 PM -06:00

--- pset6c/authorship.py
+++ pset6c/authorship.py
@@ -13,7 +13,7 @@ def word_length_histogram(text):
         for word in line_words:
             word = word.replace("'", "")
 
-            # If the length is a key, the length is increased by one
+            # If the length is a key, the length of the dictionary is increased by one
             if len(word) in list(word_hist.keys()):
                 word_histogram(len(word)) += 1
 
