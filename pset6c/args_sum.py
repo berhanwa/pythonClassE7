27e0e94b4390e0d269776cfc783e746b718f75b6
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-12-05 06:51:38 +0000

    Tue, Dec 5, 2023, 12:51 AM -06:00

--- pset6c/args_sum.py
+++ pset6c/args_sum.py
@@ -1,9 +1,10 @@
 from sys import argv
 
 def main():
-    # print([argv])
-    [for _ in argv]:
-    print(sum())
+    argv = []
+
+    for _ in argv:
+        print(sum())
     # print(f"The sum of the args is:", sum((int(argv))))
 
 main()
