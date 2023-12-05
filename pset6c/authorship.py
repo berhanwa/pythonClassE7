fb9d86dc92a8108602720914a33f53b8a7a8eb19
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-12-05 04:45:48 +0000

    Mon, Dec 4, 2023, 10:45 PM -06:00

--- pset6c/authorship.py
+++ pset6c/authorship.py
@@ -27,12 +27,14 @@ def word_length_histogram(input_text):
 
 def print_report(word_dict):
 
-    word_dict = word_length_histogram(input_text)
+    # word_dict = word_length_histogram(input_text)
 
     total_words = sum(word_dict.values())
 
     for len_word in word_histogram:
-        print('Proportion of ', len_word, '-letter words is ', word_histogram[len_word], word_histogram[len_word]/total_words)
+       print(f'Proportion of {len_word}-letter words is {word_dict[len_word]} {word_dict[len_word]/total_words:.4f}')
+
+        # print('Proportion of ', len_word, '-letter words is ', word_histogram[len_word], word_histogram[len_word]/total_words)
 
 print_report(word_dict)
 
