208b4dbb65378f9ce66b8f471bde43ade490abe2
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-12-05 07:11:09 +0000

    Tue, Dec 5, 2023, 1:11 AM -06:00

--- pset6c/args_sum.py
+++ pset6c/args_sum.py
@@ -2,17 +2,12 @@ from sys import argv
 
 def main():
 
-    # argv
-    # result_sum = sum([for _ in argv])
-    [int(argv) for argv in argv[1:]]
-    result_sum = [sum(n) for n in argv]
+    # With list comprehension, got argv converted to int and excluded the 0th index which has the script name as the value
+    ints = [int(argv) for argv in argv[1:]]
 
+    # And then used a second variable that is responsible for finding the sum of the values of from the ints variable
+    result_sum = sum(ints)
     # for _ in argv:
     print(result_sum)
-
-    # print(f"The sum of the args is:", sum(argv))
-
-    # for i in argv:
-    #     print(i)
 
 main()
