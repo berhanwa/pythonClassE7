95c61027762f86273e8ddb9dc79e8fa8d5cc1d36
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-12-05 14:30:03 +0000

    Tue, Dec 5, 2023, 8:30 AM -06:00

--- pset6c/line_numbers.py
+++ pset6c/line_numbers.py
@@ -1,9 +1,7 @@
-
+input_file_name = input("Enter a filename to read: ")
+output_file_name = input("Enter a filename to send the previous file to: ")
 
 def main():
-    input_file_name = input("Enter a filename to read: ")
-    output_file_name = input("Enter a filename to send the previous file to: ")
-
 
     # Accesses the file that the user provided the name of
     infile = open (input_file_name, "r")
