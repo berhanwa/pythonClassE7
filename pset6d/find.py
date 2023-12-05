f31dea4ccc4242af0d935c1c4d119f00d4a78feb
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-12-05 08:00:54 +0000

    Tue, Dec 5, 2023, 2:00 AM -06:00

--- pset6d/find.py
+++ pset6d/find.py
@@ -10,7 +10,7 @@ number_of_line = 0
 while len(line) != 0:
     if keyword_name in line:
         number_of_line += 1
-        print (number_of_line, line)
+        print (number_of_line, '\t', line)
     line = f.readline()
 
 f.close()
