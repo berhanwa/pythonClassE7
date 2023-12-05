c6923f324e148e619de28c34a42a73b7a6df6a8d
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-12-05 02:46:38 +0000

    Mon, Dec 4, 2023, 8:46 PM -06:00

--- pset6c/authorship.py
+++ pset6c/authorship.py
@@ -3,7 +3,10 @@ jk
 def word_length_histogram(text):
     word_histogram = {}
 
-    
+    for line in text:
+        line_words = line.split()
+        for word in line_words:
+            word = word.replace
 
 
 def print_report(hist):
