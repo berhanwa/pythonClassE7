cfdfd09401f56e42fefd49bb8504c31ba182b3c9
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-12-05 07:32:13 +0000

    Tue, Dec 5, 2023, 1:32 AM -06:00

--- pset6d/find.py
+++ pset6d/find.py
@@ -1,5 +1,14 @@
-def main():
 
-    # Opens the file and then saves the external text in this program as input_text
-    with open('romeo_and_juliet_data.txt', 'r') as f:
-        input_text = f.readlines()
+keyword_name = input("Enter a keyword to search for")
+input_file_name = input("Enter a python file to search it in")
+
+f = open (input_file_name, "r")
+
+line = f.readline()
+
+
+# def main():
+
+#     # Opens the file and then saves the external text in this program as input_text
+#     with open('romeo_and_juliet_data.txt', 'r') as f:
+#         input_text = f.readlines()
