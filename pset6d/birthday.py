14e6a85b9d8f196a76dd43d62facb0ddc9fd982a
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-12-05 16:30:08 +0000

    Tue, Dec 5, 2023, 10:30 AM -06:00

--- pset6d/birthday.py
+++ pset6d/birthday.py
@@ -3,3 +3,12 @@ from random import randint
 
 def find_matches(birthdays):
     return len(birthdays) != len(set(birthdays))
+
+
+def simulate_birthdays(simulations, students):
+
+    # Initialized the matches
+    matches = 0
+
+    for sim in range(simulations):
+        
