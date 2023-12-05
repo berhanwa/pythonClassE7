9bd2dbae1f18f112110594ab1b1a355f62c85eea
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-12-05 02:59:22 +0000

    Mon, Dec 4, 2023, 8:59 PM -06:00

--- pset6c/authorship.py
+++ pset6c/authorship.py
@@ -4,6 +4,7 @@ def word_length_histogram(text):
     word_histogram = {}
 
     for line in text:
+        
         line_words = line.split()
         for word in line_words:
             word = word.replace("'", "")
