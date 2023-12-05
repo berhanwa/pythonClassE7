6e26854bfde603e597cdb74e2ea8e157de36f884
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-12-05 07:23:44 +0000

    Tue, Dec 5, 2023, 1:23 AM -06:00

---
+++ pset6d/find.py
@@ -0,0 +1,5 @@
+def main():
+
+    # Opens the file and then saves the external text in this program as input_text
+    with open('romeo_and_juliet_data.txt', 'r') as f:
+        input_text = f.readlines()
