69a78c6ff9087b4c3f21d8829f0c7060d0918bd4
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-12-09 15:20:49 +0000

    Sat, Dec 9, 2023, 9:20 AM -06:00

--- pset7b/manager_solution.py
+++ pset7b/manager_solution.py
@@ -9,5 +9,24 @@ def employees():
         reader = csv.DictReader(file)
 
 
-        for row in header:
+        for row in reader:
             employee_name = row['FirstName'] + ' ' + row['LastName']
+            manager_name = row['Manager']
+
+            if employee_name not in ids:
+                ids[employee_name] = id
+                id += 1
+
+            if manager_name != '':
+                if manager_name not in ids:
+                    ids[manager_name] = id
+                    id += 1
+
+                print(f"{ids[employee_name]}, {ids[manager_name]}, {row['FirstName']}, {row['LastName']}")
+
+            else:
+                print(f"{ids[employee_name]}, , {row['FirstName']}, {row['LastName']}")
+
+def main():
+    employees()
+main()
