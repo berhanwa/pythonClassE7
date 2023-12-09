83f8fd5fde470a427446b4eb332089cb8542e84a
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-12-09 14:25:15 +0000

    Sat, Dec 9, 2023, 8:25 AM -06:00

--- pset7b/letters.py
+++ pset7b/letters.py
@@ -1,16 +1,18 @@
 import string
 alphabet = set(string.ascii_letters)
+used_letters = set()
 
 def missing_letters(words):
 
-    upper(alphabet)
-    used_letters = set()
     for word in words:
+        word = word.upper()
         for char in word:
             used_letters.add(char)
 
-    missing_letters = sorted(alphabet - used_letters)
-    return [letter.upper() for letter in missing_letters]
+    missing_letters = sorted(alphabet.difference(used_letters))
+    # missing_letters = sorted(alphabet - used_letters)
+    return missing_letters
+    # return [letter.upper() for letter in missing_letters]
 
 def main():
     words = [ 'Now', 'is', 'the', 'TIME']
