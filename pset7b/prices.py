4d17a29b0f5ca7e5d1bf7f95d5bc05ce52f50e0b
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-12-09 15:49:17 +0000

    Sat, Dec 9, 2023, 9:49 AM -06:00

---
+++ pset7b/prices.py
@@ -0,0 +1,13 @@
+import csv
+import os
+import requests
+import sys
+
+if len(sys.argv) != 2:
+    sys.exit("Usage: python quote.py STOCK-SYMBOL")
+symbol = sys.argv[1]
+
+# Get API_KEY
+API_KEY = os.getenv("API_KEY")
+if not API_KEY:
+    sys.exit("Missing API_KEY")
