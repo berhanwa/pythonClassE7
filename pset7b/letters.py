cc79f1531d0113ad1a2122c80c666b8dcc3c1e2a
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-12-09 14:49:21 +0000

    Sat, Dec 9, 2023, 8:49 AM -06:00

--- pset7b/letters.py
+++ pset7b/letters.py
@@ -10,6 +10,7 @@ def missing_letters(words):
         for char in word:
             if  char.isalpha():
                 used_letters.add(char)
+                
             # alphabet_cap = alphabet.upper()
     missing_letters = sorted(alphabet.difference(used_letters))
     # missing_letters = sorted(alphabet - used_letters)
