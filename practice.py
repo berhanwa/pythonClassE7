240e18afb2993ef20041fb249f9d48621e06dbc9
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-12-09 00:36:38 +0000

    Fri, Dec 8, 2023, 6:36 PM -06:00

--- practice.py
+++ practice.py
@@ -201,7 +201,11 @@
 
 # print(recurse([4, 9, 'blah', 3.21]))
 
-y = set()
-y.add('a')
-y.add('a')
+# y = set()
+# y.add('a')
+# y.add('a')
+# print(y)
+
+y = { 'b', 'a', 'b', 'c' }
+y.remove('b')
 print(y)
