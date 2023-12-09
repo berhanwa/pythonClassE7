12ce1b07e00d2b7f19ead3329e97aabc4aa8e193
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-12-09 08:28:22 +0000

    Sat, Dec 9, 2023, 2:28 AM -06:00

--- pset7b/letters.py
+++ pset7b/letters.py
@@ -4,4 +4,12 @@ print (alphabet)
 
 
 def missing_letters(words):
-    
+
+    used_letters = set()
+
+    for word in words:
+        for char in word:
+            used_letter.add(char)
+
+    missing_letters = sorted(list(all_letters - used_letters))
+    return missing_letters
