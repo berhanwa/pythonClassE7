7c8128b809c0ce9485098b1d5b6e39c63a82f30c
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-12-09 16:20:46 +0000

    Sat, Dec 9, 2023, 10:20 AM -06:00

--- pset7b/prices.py
+++ pset7b/prices.py
@@ -1,8 +1,10 @@
+# Based a lot of this program structure from the class file quote.py
 import csv
 import os
 import requests
 import sys
 
+# Checked for the appropriate formatting from the command line and specified what it was if the length of input was not long enough
 if len(sys.argv) != 2:
     sys.exit("Usage: python quote.py STOCK-SYMBOL")
 symbol = sys.argv[1]
@@ -12,10 +14,12 @@ API_KEY = os.getenv("API_KEY")
 if not API_KEY:
     sys.exit("Missing API_KEY")
 
+# With the API, accessed the stock data as a csv file
 url = f"https://www.alphavantage.co/query?apikey={API_KEY}&datatype=csv&function=TIME_SERIES_INTRADAY&interval=5min&symbol={symbol}"
 response = requests.get(url)
 print(response.text)
 
+Parsed the reader as a dictionary,
 reader = csv.DictReader(response.text.splitlines())
 
 row = next(reader)
