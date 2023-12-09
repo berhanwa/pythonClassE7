ab3ff310198efcaa685738cc9815a3bc336ab057
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-12-09 15:42:53 +0000

    Sat, Dec 9, 2023, 9:42 AM -06:00

--- pset7b/manager_solution.py
+++ pset7b/manager_solution.py
@@ -9,17 +9,17 @@ def employees():
     with open("employees.csv", "r") as file:
         reader = csv.DictReader(file)
 
-        # Then, iterated through each row in the csv to assign the employee and manager names. For the employees, concatenated both names into the variable.
+        # Then, iterated through each row in the csv to gather the employee and manager names. For the employees, concatenated both names into the variable to get their full names.
         for row in reader:
             employee_name = row['FirstName'] + ' ' + row['LastName']
             manager_name = row['Manager']
 
-            # Assigned id to employee if that has already not been done so
+            # Assigned id to employee if that has already not been done so, and if found, assigns an id to their name and incremented the id counter
             if employee_name not in ids:
                 ids[employee_name] = id
                 id += 1
 
-            # And assigned id to manager_name if that hasn't been done either
+            # And assigned id to manager_name if that hasn't been done either and if found, assigns an id to their name and incremented the id counter
             if manager_name != '':
                 if manager_name not in ids:
                     ids[manager_name] = id
