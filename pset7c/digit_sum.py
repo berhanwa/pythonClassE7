0218b10520f7e0781c5163265675f0357b366424
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-12-11 22:23:24 +0000

    Mon, Dec 11, 2023, 4:23 PM -06:00

--- pset7c/digit_sum.py
+++ pset7c/digit_sum.py
@@ -1,17 +1,19 @@
-
 # Step one: Make the function work as needed
 # Step two: Now make it recursive
+
 def digit_sum(n):
 
     if n < 10:
         return n
 
     else:
+        # Turned n to a string and assigned it to variable in order to iterate through it
         total = str(n)
-        # for digit in n:
+        # Then turned the indices within total into a list of integers, in order to use the sum function
         digits = [int(i) for i in total]
+
         return sum(digits)
-    # digit_sum(n)
+
 
 
 print(digit_sum(3456))
