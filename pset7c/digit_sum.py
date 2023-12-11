f424342406fbe8c1c9f8841c037169e2b2b6cfb1
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-12-11 22:34:14 +0000

    Mon, Dec 11, 2023, 4:34 PM -06:00

--- pset7c/digit_sum.py
+++ pset7c/digit_sum.py
@@ -6,20 +6,16 @@ def digit_sum(n):
     if n < 10:
         return n
 
-    elif n > 10:
-        # Turned n to a string and assigned it to variable in order to iterate through it
-        total = str(n)
-        # Then turned the indices within total into a list of integers, in order to use the sum function
-        digits = [int(i) for i in total]
-
-        return digit_sum(digits[1:])
+    else:
+        return n % 10 + digit_sum(n // 10)
 
 # Supposed to make it recursive by removing an index until n meets base case
     # else:
     #     return digit_sum(digits[1:])
 
 print(digit_sum(3456))
-
+print(digit_sum(3))
+print(digit_sum(23))
 
 # def digit_sum(n):
 
