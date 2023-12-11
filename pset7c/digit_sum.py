8061bf5b2702843cb8e575edbd1e319431a3c7ad
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-12-11 21:52:55 +0000

    Mon, Dec 11, 2023, 3:52 PM -06:00

--- pset7c/digit_sum.py
+++ pset7c/digit_sum.py
@@ -1,12 +1,13 @@
 def digit_sum(n):
 
     total = 0
-    
+
     if n < 10:
         return n
     else:
-        for i in range(total):
-            return sum(i)
+        for digit in range(n):
+                int(digit) += total
+            return sum(total)
 
 
 print(digit_sum(3456))
