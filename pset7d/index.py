b71f0e6c4113f10e26280b992f710fc922381316
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-12-12 04:17:09 +0000

    Mon, Dec 11, 2023, 10:17 PM -06:00

--- pset7d/index.py
+++ pset7d/index.py
@@ -1,7 +1,9 @@
 def index_of(text, a_string):
     if not(a_string in text):
         return -1
+    elif:
+        return text.startswith(a_string) +
     else:
-        return text.startswith(a_string) + index_of(text[1:], a_string)
+        return index_of(text[1:], a_string)
 
-print(index_of("Mississippi", "sip"))
+print(s.index_of("Mississippi", "sip"))
