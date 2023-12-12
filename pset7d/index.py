2dff8858be3928e284ad9d1b9e4682476f17141e
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-12-12 04:36:17 +0000

    Mon, Dec 11, 2023, 10:36 PM -06:00

--- pset7d/index.py
+++ pset7d/index.py
@@ -1,10 +1,12 @@
+After detailing the base case from the pset handout, checked if the length of the two parameters are equal, 
 def index_of(text, a_string):
     if not(a_string in text):
         return -1
     elif len(a_string) == len(text):
         return 0
 
-    Recursion by means of making 0
+    # Recursively iterated through text by means of making it one character shorter (removing the 0th index position)
+    # and then incremented by 1 to account for the removed element from the recursion
     else:
         return index_of(text[1:], a_string) + 1
 
