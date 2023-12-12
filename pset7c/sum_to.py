909d33fb25a3d28f18772c41d88e55c7387c2994
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-12-12 02:47:05 +0000

    Mon, Dec 11, 2023, 8:47 PM -06:00

--- pset7c/sum_to.py
+++ pset7c/sum_to.py
@@ -1,7 +1,15 @@
 def sum_to(n):
     if n == 0:
         return 0
+    elif n < 0:
+        print("Error")
     else:
         return 1/n + sum_to(n-1)
 
-print(sum_to(2))
+
+def main():
+    print(sum_to(2))
+    print(sum_to(3))
+    print(sum_to(0))
+    print(sum_to(-12))
+main()
