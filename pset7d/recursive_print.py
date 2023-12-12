216d8ac138217d71e15e2fac5154d4d79f325ef3
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-12-12 16:57:55 +0000

    Tue, Dec 12, 2023, 10:57 AM -06:00

--- pset7d/recursive_print.py
+++ pset7d/recursive_print.py
@@ -3,23 +3,29 @@
 
 # Started by assigning variables to lists by decimal type
 def print_number(n):
-    ones_numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+    # ones_numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
     tens_numbers = ["", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
-    teens_numbers = ["", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
+    first_twenty = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+                    "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen",
+                    "eighteen", "nineteen"]
+    # teens_numbers = ["", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
 
     # In this if statement part, defined the different digit lengths and assigned the appropriate variable from above and what should be printed out
     if n < 0:
         print("negative", end = " ")
         n = abs(n)
 
-    if n < 10:
-        print(ones_numbers[n], end = " ")
+    # if n < 10:
+    #     print(ones_numbers[n], end = " ")
 
-    if n == 10:
-        print(tens_numbers[n - 10 + 1], end = " ")
+    # if n == 10:
+    #     print(tens_numbers[n - 10 + 1], end = " ")
 
-    elif n < 20:
-        print(teens_numbers[n - 10], end=" ")
+    # elif n < 20:
+    #     print(teens_numbers[n - 10], end=" ")
+
+    if n < 20:
+        print(first_twenty[n], end=" ")
 
     elif n <= 100:
         print(tens_numbers[n // 10], end = " ")
