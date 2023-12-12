a9392504283be1ae5dde442a552bd382c7356aa8
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-12-12 03:31:48 +0000

    Mon, Dec 11, 2023, 9:31 PM -06:00

--- pset7c/recursive_triangle.py
+++ pset7c/recursive_triangle.py
@@ -1,7 +1,7 @@
 def print_triangle (sideLength):
+    print ( "[]"* sideLength)
     if sideLength < 1 :
         return
     print_triangle (sideLength-1)
-    print ( "[]"* sideLength)
 
 print(print_triangle (4))
