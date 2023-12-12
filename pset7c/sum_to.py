a419dc71586b86708e56e945a49c91aeebd826ec
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-12-12 02:51:50 +0000

    Mon, Dec 11, 2023, 8:51 PM -06:00

--- pset7c/sum_to.py
+++ pset7c/sum_to.py
@@ -1,8 +1,11 @@
+# Set the base case to return 0 if that is what n is, and the error message if n is a negative number
 def sum_to(n):
     if n == 0:
         return 0
     elif n < 0:
-        print("Error")
+        return None
+
+    # Here, 
     else:
         return 1/n + sum_to(n-1)
 
@@ -13,3 +16,6 @@ def main():
     print(sum_to(0))
     print(sum_to(-12))
 main()
+
+
+ # print("Error")
