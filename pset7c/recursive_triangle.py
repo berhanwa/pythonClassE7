9e0241878ca7d963260a3ba29204335e56b349d8
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-12-12 03:36:29 +0000

    Mon, Dec 11, 2023, 9:36 PM -06:00

--- pset7c/recursive_triangle.py
+++ pset7c/recursive_triangle.py
@@ -1,10 +1,11 @@
-
-
-Modified the function so that the print statment with the []s gets printed out BEFORE the recursion happens
+# Modified the function so that the print statment with the []s gets printed out BEFORE the recursion happens
 def print_triangle (sideLength):
     print ( "[]"* sideLength)
+
+    # Here, I retained the recursion that allows the []s to be printed out all the way until sideLength reaches 0
     if sideLength < 1 :
         return
     print_triangle (sideLength-1)
+
 
 print(print_triangle (4))
