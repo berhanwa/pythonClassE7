a7bca84b13f12d44dd3724f1c3b01ffd4c7278ed
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-12-12 04:32:21 +0000

    Mon, Dec 11, 2023, 10:32 PM -06:00

--- pset7d/index.py
+++ pset7d/index.py
@@ -3,6 +3,8 @@ def index_of(text, a_string):
         return -1
     elif len(a_string) == len(text):
         return 0
+
+    Recursion by means of making 0
     else:
         return index_of(text[1:], a_string) + 1
 
