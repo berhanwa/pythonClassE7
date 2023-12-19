b60cba047e707cfbe1127879d52a7197a361e39a
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-12-19 16:35:27 +0000

    Tue, Dec 19, 2023, 10:35 AM -06:00

--- termproj/my_wordle.py
+++ termproj/my_wordle.py
@@ -4,8 +4,8 @@
 
 # Return the feedback per round until the final 6th round to let users know if they won or lost
 
-from random import *
-from sys import argv
+
+
 
 def load_words():
     words = []
@@ -21,16 +21,57 @@ def play(word):
     #Scenario Number One - User guesses the correct word and wins
     if guess == word:
         print("🟢🟢🟢🟢🟢")
+        for i in range(0, len(word)):
+            print(word[i].upper(), end = " ")
+        print()
+        return True
+    else:
 
+        result = ""
+        letters = " "
+        for i in range(0, len(word)):
 
+            #Scenario Number Two - User guesses a word with letters in the correct positions
+            if guess[i] == word[i]:
+                result += "🟢"
+                letters += guess[i].upper() + " "
 
-    with open(argv[1], 'r') as file:
+            #Scenario Number Three - User guesses a word with letters not in the correct positions
+            elif guess[i] in word:
+                result += "🟡"
+                letters += guess[i].upper() + " "
 
-        for word in file:
-            word_list.append( word[0:len(word)-1] )
-    # print(word_list)
-    winning_word = word_list[ randint(0, len(word_list))]
+            #Scenario Number Four - User guesses a word with incorrect letters
+            else:
+                result += "🔴"
+                letters += guess[i].upper() + " "
 
-print("Wordle Game Instructions")
+        print(result)
+        print(letters)
+
+def main():
+    import random
+    words = load_words()
+
+    word = random.choice(words).lower()
+
+    guesses = 6
+    while guesses > 0:
+        if play(word):
+            print("You win")
+            break
+        else:
+            guesses -= 1
+            print("You have %d guesses remaining" % guesses)
+            print()
+            print("_________________________________")
+
+    if guesses == 0:
+        word[0] = word[0].upper()
+        print("You lost. The correct word was %s." %word)
+
+main()
+
+# print("Wordle Game Instructions")
 
 
