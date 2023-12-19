ce1ff9de3c0c4b5397b55ec3be2465f9a07cbd0a
Bethlehem Berhanu <70812449+berhanwa@users.noreply.github.com>
2023-12-19 00:26:26 +0000

    Mon, Dec 18, 2023, 6:26 PM -06:00

---
+++ termproj/my_wordle.py
@@ -0,0 +1,7 @@
+# Program begins by asking users to guess the word (argv)
+
+# Iterate through each of the 6 rounds of guesses and check each char, if it's in the word, in the right place or not in there
+
+# Return the feedback per round until the final 6th round to let users know if they won or lost
+
+def
